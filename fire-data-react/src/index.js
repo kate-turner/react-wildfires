07c774e93cb9966cbb712f41bb53a144d4e3b75e
Erica Bruenton <ercfit@gmail.com>
2018-08-07 10:44:48 -0600

    eyeroll

--- fire-data-react/src/index.js
+++ fire-data-react/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'));
 registerServiceWorker();
